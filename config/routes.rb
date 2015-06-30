bd3f0f95245d63ef17bc2ee55b22bf7f17020e76
Moises Zaragoza <mZaragoza@circletechfl.com>
2015-06-30 17:21:35 -0400

    create users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
 
+  devise_for :users, :controllers => {
+    registrations: 'users/registrations',
+    :sessions => "users/sessions",
+    :passwords => 'users/passwords',
+    :confirmations => 'users/confirmations'
+  }
+
+  authenticate :user do
+    namespace :users do
+    end
+  end
+
   root 'pages#index'
 end
