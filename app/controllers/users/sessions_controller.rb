9559a740a20a8afd3d045fa8f078282452b47cb4
Moises Zaragoza <mZaragoza@circletechfl.com>
2015-07-04 10:00:51 -0400

    user can login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,11 +2,7 @@ class Users::SessionsController < Devise::SessionsController
   layout 'application'
   private
   def after_sign_in_path_for(resource)
-    if resource.is_admin
-      users_dashboard_path
-    else
-      users_consumers_path
-    end
+    users_dashboard_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
