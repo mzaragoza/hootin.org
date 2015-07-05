645aee48246ffe5da06e23f3ef9b35bf2416ddd7
Moises Zaragoza <mZaragoza@circletechfl.com>
2015-07-04 20:14:44 -0400

    updates

--- app/controllers/api/v1/twilio_controller.rb
+++ app/controllers/api/v1/twilio_controller.rb
@@ -3,19 +3,20 @@ class Api::V1::TwilioController < ApiController
 
   def inbound_call
     user = User.where(available: true).shuffle.first
-    call = Call.new
-    call.from = params[:From]
-    call.to = user.id
-    call.save
     if user
       response = Twilio::TwiML::Response.new do |r|
-
         r.Dial callerId: '' do |d|
           d.Number user.phone
         end
+
       end
       user.available = false
       user.save
+      call = Call.new
+      call.from = params[:From] || ' '
+      call.to = user.id
+      call.answered =  ' '
+      call.save
       render_twiml response
     else
       response = Twilio::TwiML::Response.new do |r|
@@ -25,6 +26,11 @@ class Api::V1::TwilioController < ApiController
         r.Say " I am sorry but there is noone available to take your call", :voice => 'alice' #, language: "it-IT"
         r.Play 'http://linode.rabasa.com/cantina.mp3'
       end
+      call = Call.new
+      call.from = params[:From] || ' '
+      call.to = ' '
+      call.answered =  ' '
+      call.save
       render_twiml response
     end
   end
